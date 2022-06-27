6651def13413537a1c65dec76bc4e43dbe56ec81
Rodrigo <rlvenancio@gmail.com>
2022-06-27 16:27:46 -0300

    Altera métodos da classe Time

--- classes/time.py
+++ classes/time.py
@@ -4,79 +4,49 @@ class Jogador:
         self.__gols_marcados = 0
         return self.__nome, self.__gols_marcados
 
-class Time:
-    def __init__(self):
-        self.__nome_do_time = input("Digite o nome do time: ").capitalize().strip()
-        self.__tecnico = input("Digite o nome do técnico: ").capitalize().strip()
-        self.__escalacao = []
+class Time(Jogador):
+    def __init__(self, time, tecnico):
+        self.__nome_do_time = time.title().strip()
+        self.__tecnico = tecnico.title().strip()
+        self.__escalacao = {}
         self.__saldo_de_gols = 0
-        self.__artileiro = {}
 
-    def apresenta_time(self):
-        return print(
-            f"Apresento o time {self.__nome_do_time}, "
-            f"seu técino o sr(a) {self.__tecnico}, "
-            f"e os jogadores {self.__escalacao}, "
-            f"o saldo de gols do time é {self.__saldo_de_gols}, "
-            f"os jogadores que marcaram gol foram {self.__artileiro}"
-        )
+    def inclui_jogador(self, nome):
+        jogador = super().__init__(nome)
+        self.__escalacao[jogador[0]] = jogador[1]
+        return "Jogador incluído com sucesso!"
 
-    def dados_do_time(self):
-        dados_do_time = {}
-        dados_do_time[self.__nome_do_time] = self.__escalacao
-        return dados_do_time
+    def exclui_jogador(self, nome):
+        jogador = nome.title().strip()
+        if jogador in self.__escalacao.keys():
+            self.__escalacao.pop(jogador)
+        return "Jogador excluído com sucesso!"
 
-    def inclui_jogadores(self):
-        validacao1 = input("Sabe quantos jogadores serão cadastrado? \"S\" ou \"N\"").upper()
-        if validacao1=="S":
-            quantidade = int(input("Quantos jogadores serão inseridos:"))
-            cont = 0
-            while quantidade > cont:
-                jogador = input("Digite o nome do jogador").capitalize().strip()
-                while jogador == "":
-                    print("Favor digitar um nome válido!")
-                    jogador = input("Digite o nome do jogador").capitalize().strip()
-                self.__escalacao.append(jogador)
-                cont += 1
-        else:
-            jogador = input("Digite o nome do jogador").capitalize().strip()
-            while jogador == "":
-                print("Favor digitar um nome válido!")
-                jogador = input("Digite o nome do jogador").capitalize().strip()
-            self.__escalacao.append(jogador)
-            Resp = input("Deseja incluir outro jogador? \"S\" ou \"N\"").upper()
-            while Resp == "S":
-                jogador = input("Digite o nome do jogador").capitalize().strip()
-                while jogador == "":
-                    print("Favor digitar um nome válido!")
-                    jogador = input("Digite o nome do jogador").capitalize().strip()
-                self.__escalacao.append(jogador)
-                Resp = input("Deseja incluir outro jogador? \"S\" ou \"N\"").upper()
+    def jogadores(self):
+        for jogador in self.__escalacao.keys():
+            print(jogador)
 
-    def exclui_jogador(self):
-        jogador_a_excluir = input("Digite o jogador que será excluído do time: ").capitalize().strip()
-        if jogador_a_excluir in self.__escalacao:
-            self.__escalacao.remove(jogador_a_excluir)
-            return "Jogador excluído com sucesso!"
-        else:
-            return "Jogador não está na lista!"
-
-    def alterar_tecnico(self):
-        novo_tecnico = input("Digite o nome do novo técnico: ").capitalize().strip()
-        self.__tecnico = novo_tecnico
-
-    def marca_gol(self):
-        nome_do_jogador = input("Qual jogador marcou gol: ").capitalize().strip()
-        if nome_do_jogador in self.__escalacao:
-            total_gols_marcados = int(input(f"Quantos gols {nome_do_jogador} marcou nesse jogo: "))
-            self.__artileiro[nome_do_jogador] = total_gols_marcados
+    def marca_gol(self, nome_jogador):
+        jogador = nome_jogador.title().strip()
+        if jogador in self.__escalacao.keys():
+            self.__escalacao[jogador] += 1
             self.__saldo_de_gols += 1
+            return "Gol marcado com sucesso!"
         else:
-            print(f'O {nome_do_jogador} não joga neste time')
+            return "Jogador não é deste time!"
+
+    def anula_gol(self, nome_jogador):
+        jogador = nome_jogador.title().strip()
+        if jogador in self.__escalacao.keys():
+            if self.__escalacao[jogador] > 0:
+                self.__escalacao[jogador] -= 1
+                self.__saldo_de_gols -= 1
+                return "Gol anulado com sucesso!"
+            else:
+                return "Jogador não tem gol a ser anulado!"
+        else:
+            return "Jogador não é deste time!"
 
     def sofre_gol(self):
-        gol_sofrido = 1
-        self.__saldo_de_gols -= gol_sofrido
-
-    def artileiro(self):
-        return self.__artileiro
+        self.__saldo_de_gols -= 1
+        return "Anotado com sucesso!"
